48adff4a3598aa7ee96b90c7abfffcbe2b3c5995
JudysImpostorGame <mohamadjudy86@gmail.com>
2025-05-17 00:03:36 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('language').addEventListener('change', function () {
-  const selectedLang = this.value;
-  console.log("Sprache gewechselt zu:", selectedLang);
-  // Hier könntest du z.B. Inhalte dynamisch übersetzen
-});
-
-document.getElementById('startButton').addEventListener('click', function () {
-  window.location.href = "modus-auswahl.html"; // Weiter zur nächsten Seite
-});
